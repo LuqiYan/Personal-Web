d4c50b98fb4622c2a059db8e2646b8013d04fb52
Luqi Yan <ylq.7719@gmail.com>
2015-10-30 14:33:30 -0700

    n

--- js/agency.js
+++ js/agency.js
@@ -18,9 +18,9 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
 });
--- js/google_map.js
+++ js/google_map.js
@@ -1,17 +1,31 @@
-function init_map() {
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        center: new google.maps.LatLng(37.320102,-121.99703899999997),
+
+var myCenter = new google.maps.LatLng(37.320102, -121.997039);
+var marker;
+
+function initialize()
+{
+    var mapProp = {
+        center: myCenter,
         zoom: 9,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(37.320102, -121.99703899999997)});
-    var infowindow = new google.maps.InfoWindow({content:"<b></b><br/>18594 loree ave<br/> cupertino" });
+
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        animation: google.maps.Animation.BOUNCE
+    });
+
+    marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({content:"<b></b><br/>18594 Loree Ave, Cupertino, CA" });
+
     google.maps.event.addListener(marker, "click", function(){
         infowindow.open(map,marker);
     });
     infowindow.open(map,marker);
 }
-google.maps.event.addDomListener(window, 'load', init_map);
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
